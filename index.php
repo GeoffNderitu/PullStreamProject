65a48bbf89531d6ddf6b2869ad14b8d0246615d4
GeoffNderitu <geoffreynderitun@gmail.com>
2024-03-04 13:50:34 +0300

    A little Streaming Project

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to PullStream</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="main_body">
+    <div class="header">
+        <h1 class="header_text">
+            WELCOME TO <span class="logo_1">PULL</span><span class="logo_2">STREAM</span>
+        </h1>
+    </div>
+    <!-- Navigation Bar -->
+<div class="navbar">
+    <a href="#">Home</a>
+    <a href="#">Browse</a>
+    <a href="#">Trending</a>
+    <a href="#">Categories</a>
+    <a href="#">SignUp</a>
+                <div class="search_form">
+                    <form action="">
+                        <input type="text" placeholder="search here" class="search_box">
+                        <button type="submit" class="search_btn"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+</div>
+    <div class="container">
+        <div class="row">
+            <div class="column-right">
+
+            </div>
+            <div class="column-left">
+
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
